65ce24b1ed12cdded2677ee7dee8e79f8b2c6a41
bermeternazarova <eaviva38@gmail.com>
2023-02-01 15:06:49 +0600

    correct  some mistakes

--- app/src/main/java/com/example/textsizekotlin_month05/MainActivity.kt
+++ app/src/main/java/com/example/textsizekotlin_month05/MainActivity.kt
@@ -2,16 +2,12 @@ package com.example.textsizekotlin_month05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.viewModels
 import com.example.textsizekotlin_month05.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var diffPlus = 10.0f
-    private var diffMinus = 0.0f
-    private lateinit var viewModel :ViewModelText
+    private val viewModel: ViewModelText by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,32 +16,18 @@ class MainActivity : AppCompatActivity() {
         initializeViewModel()
         initListeners()
     }
-
     private fun initializeViewModel() {
-        viewModel = ViewModelProvider(this)[ViewModelText::class.java]
+      //  viewModel = ViewModelProvider(this)[ViewModelText::class.java]
     }
     private fun initListeners() {
         viewModel.count.observe(this) { count ->
             binding.tv.text = count.toString()
         }
         binding.btnIncrease.setOnClickListener {
-            if (binding.tv.text == diffPlus.toString())snackBarShow(it)
-            else increase()
+            viewModel.increase(it)
         }
         binding.btnDecrease.setOnClickListener {
-            if (binding.tv.text==diffMinus.toString())snackBarShow(it)
-            else decrease()
+            viewModel.decrease(it)
         }
-    }
-      private fun snackBarShow(view: View){
-          val snackBar = Snackbar.make(view, "Вы достигли предела!",
-              Snackbar.LENGTH_LONG)
-          snackBar.show()
-    }
-      private fun increase(){
-     viewModel.increase()
- }
-      private fun decrease(){
-        viewModel.decrease()
     }
 }
--- app/src/main/java/com/example/textsizekotlin_month05/ViewModelText.kt
+++ app/src/main/java/com/example/textsizekotlin_month05/ViewModelText.kt
@@ -1,20 +1,43 @@
 package com.example.textsizekotlin_month05
 
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ViewModelText :ViewModel (){
+@HiltViewModel
+class ViewModelText @Inject constructor(): ViewModel (){
     private  val _count = MutableLiveData<Float>()
     val count : LiveData<Float> =_count
-    private var diff = 0.0f
 
-    fun increase(){
-        diff ++
-        _count.value = diff
+    private var _snack = MutableLiveData<Snackbar>()
+    val snack : LiveData<Snackbar> = _snack
+
+    private var diff = 0.0f
+    private var diffPlus = 10.0f
+    private var diffMinus = 0.0f
+
+    fun increase(view: View){
+        val snackBar = Snackbar.make(view,"Вы достигли предела",Snackbar.LENGTH_SHORT)
+        if (diff== diffPlus) {
+            snackBar.show()
+            _snack.value = snackBar
+        }else{
+            diff ++
+            _count.value = diff
+        }
     }
-    fun decrease(){
-        diff --
-        _count.value = diff
+    fun decrease(view: View){
+        val snackBar = Snackbar.make(view,"Вы достигли предела",Snackbar.LENGTH_SHORT)
+        if (diff == diffMinus){
+            snackBar.show()
+            _snack.value=snackBar
+        }else{
+            diff --
+            _count.value = diff
+        }
     }
 }
